7e66e1b9f8818bd130fd278c5eed226bcc86013e
Leo Wilson <leowilson.xlw123@gmail.com>
2019-05-12 17:02:05 -0700

    Fix the python code

--- src/python/python.py
+++ src/python/python.py
@@ -1,12 +1,12 @@
 def isPrime(n: int) -> bool:
-    for i in range(2, int(n / 2)):
+    for i in range(2, n):
         if n % i == 0:
             return False
     return True
 
 p: int = 2
 
-for i in range(1001):
+for i in range(1000):
     while not isPrime(p):
         p += 1
     print(p)
