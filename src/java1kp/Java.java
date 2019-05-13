1922714979aef1936cdfa63f6d4013d86e46f4ed
Leo Wilson <leowilson.xlw123@gmail.com>
2019-05-12 17:16:18 -0700

    Finish Java code

--- src/java1kp/Java.java
+++ src/java1kp/Java.java
@@ -7,18 +7,22 @@ public class Java {
 	ArrayList<Integer> s = new ArrayList<Integer>();
 	int p = 2;
 	for (int i = 0; i < 1000; i++) {
-	    boolean prime = true;
-	    for (int j : s) {
-		if (p % j == 0) {
-		    prime = false;
+	    while (true) {
+		boolean prime = true;
+		for (int j : s) {
+		    if (p % j == 0) {
+			prime = false;
+			break;
+		    }
+		}
+		if (prime) {
+		    s.add(p);
+		    System.out.println(p);
+		    p++;
 		    break;
 		}
+		p++;
 	    }
-	    if (prime) {
-		s.add(p);
-		System.out.println(p);
-	    }
-	    p++;
 	}
     }
 }
